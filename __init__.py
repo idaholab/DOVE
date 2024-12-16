a1c6982b8f9d809a004f3106df4cfba04f0c5a7c
Caleb Sitton <caleb.sitton@inl.gov>
2024-12-16 15:03:30 -0700

    Changed to using os.pardir

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ import importlib
 import os
 import sys
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir))
 
 import DOVE.src._utils as dutils
 
