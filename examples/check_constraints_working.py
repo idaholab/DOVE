fe93f8428c1c8f4fe9f70d61e835448b72df1c91
Caleb Sitton <caleb.sitton@inl.gov>
2025-06-18 17:00:43 -0600

    test(examples): add library of functions to check constraints and new example

---
+++ examples/check_constraints_working.py
@@ -0,0 +1,163 @@
+# Copyright 2024, Battelle Energy Alliance, LLC
+# ALL RIGHTS RESERVED
+"""
+Defines some functions that can be used to check whether constraints are working properly
+"""
+
+import numpy as np
+
+
+def max_capacity_profile_working(sys, results, comp_name, column_name):
+    comp = sys.comp_map[comp_name]
+    activity_profile = results.loc[:, column_name].values
+
+    if (abs(activity_profile) < comp.max_capacity_profile + 1e-6).all():
+        print(f"✅ Max capacity profile constraint working correctly for {comp_name}!")
+    else:
+        print(f"❌ Max capacity profile constraints violated for {comp_name}!")
+
+    new_max_capacity_profile = np.multiply(comp.max_capacity_profile, 2)
+    confirm_economic_motivation(
+        sys, results, comp_name, "max_capacity_profile", new_max_capacity_profile
+    )
+    print()
+
+
+def min_capacity_profile_working(sys, results, comp_name, column_name):
+    comp = sys.comp_map[comp_name]
+    activity_profile = results.loc[:, column_name].values
+
+    if (abs(activity_profile) > comp.min_capacity_profile - 1e-6).all():
+        print(f"✅ Min capacity profile constraint working correctly for {comp_name}!")
+    else:
+        print(f"❌ Min capacity profile constraint violated for {comp_name}!")
+
+    new_min_capacity_profile = np.full(len(sys.time_index), 0)
+    confirm_economic_motivation(
+        sys, results, comp_name, "min_capacity_profile", new_min_capacity_profile
+    )
+    print()
+
+
+def ramp_limit_working(sys, results, comp_name, column_name):
+    comp = sys.comp_map[comp_name]
+    activity_profile = results.loc[:, column_name].values
+
+    ramp_rates = np.abs(np.diff(activity_profile))
+    if np.all(ramp_rates <= (comp.ramp_limit * np.max(comp.max_capacity_profile)) + 1e-6):
+        print(f"✅ Ramp rate limit constraint working correctly for {comp_name}!")
+    else:
+        print(f"❌ Ramp rate limit constraint violated for {comp_name}!")
+
+    new_ramp_limit = 1.0
+    confirm_economic_motivation(sys, results, comp_name, "ramp_limit", new_ramp_limit)
+    print()
+
+
+def ramp_freq_working(sys, results, comp_name, column_name):
+    comp = sys.comp_map[comp_name]
+    activity_profile = results.loc[:, column_name].values
+    ramp_rates = np.abs(np.diff(activity_profile))
+
+    # Count ramp events
+    # Define a significant ramp as >5% of max capacity
+    ramp_threshold = 0.05 * np.max(comp.max_capacity_profile)
+    ramp_events = ramp_rates > ramp_threshold
+    ramps_per_window = []
+    for i in range(len(ramp_events) - comp.ramp_freq + 1):
+        window = ramp_events[i : i + comp.ramp_freq]
+        ramps_per_window.append(np.sum(window))
+
+    if np.max(ramps_per_window) <= 1:
+        print(f"✅ Ramp frequency constraint working correctly for {comp_name}!")
+    else:
+        print(f"❌ Ramp frequency constraint violated for {comp_name}!")
+
+    new_ramp_freq = 0.0
+    confirm_economic_motivation(sys, results, comp_name, "ramp_freq", new_ramp_freq)
+    print()
+
+
+def max_charge_rate_working(sys, results, comp_name):
+    comp = sys.comp_map[comp_name]
+    charge = results.loc[:, f"{comp_name}_charge"].values
+
+    if np.max(charge) < comp.max_charge_rate * np.max(comp.max_capacity_profile) + 1e-6:
+        print(f"✅ Max charge constraint working correctly for {comp_name}!")
+    else:
+        print(f"❌ Max charge constraint violated for {comp_name}!")
+
+    new_max_charge_rate = 1.0
+    confirm_economic_motivation(sys, results, comp_name, "max_charge_rate", new_max_charge_rate)
+    print()
+
+
+def max_discharge_rate_working(sys, results, comp_name):
+    comp = sys.comp_map[comp_name]
+    discharge = results.loc[:, f"{comp_name}_discharge"].values
+
+    if np.max(discharge) < comp.max_discharge_rate * np.max(comp.max_capacity_profile) + 1e-6:
+        print(f"✅ Max discharge constraint working correctly for {comp_name}!")
+    else:
+        print(f"❌ Max discharge constraint violated for {comp_name}!")
+
+    new_max_discharge_rate = 1.0
+    confirm_economic_motivation(
+        sys, results, comp_name, "max_discharge_rate", new_max_discharge_rate
+    )
+    print()
+
+
+def rte_working(sys, results, comp_name):
+    # Note: implicitly checks storage balance as well
+    comp = sys.comp_map[comp_name]
+    soc = results.loc[:, f"{comp_name}_SOC"].values
+    charge = results.loc[:, f"{comp_name}_charge"].values
+    discharge = results.loc[:, f"{comp_name}_discharge"].values
+
+    soc_diff = np.concatenate(
+        (np.array([soc[0] - comp.initial_stored * np.max(comp.max_capacity_profile)]), np.diff(soc))
+    )
+    if np.all(abs(soc_diff - charge * comp.rte**0.5 + discharge / comp.rte**0.5) < 1e-6):
+        print(f"✅ RTE constraint working correctly for {comp_name}!")
+    else:
+        print(f"❌ RTE constraint violated for {comp_name}!")
+
+    new_rte = 1.0
+    confirm_economic_motivation(sys, results, comp_name, "rte", new_rte)
+    print()
+
+
+def periodic_storage_working(sys, results, comp_name):
+    comp = sys.comp_map[comp_name]
+    soc = results.loc[:, f"{comp_name}_SOC"].values
+
+    if abs(soc[-1] - (comp.initial_stored * np.max(comp.max_capacity_profile))) < 1e-6:
+        print(f"✅ Periodic storage constraint working correctly for {comp_name}!")
+    else:
+        print(f"❌ Periodic storage constraint violated for {comp_name}!")
+
+    new_periodic_level = False
+    confirm_economic_motivation(sys, results, comp_name, "periodic_level", new_periodic_level)
+    print()
+
+
+def confirm_economic_motivation(sys, orig_results, comp_name, attribute, new_value):
+    comp = sys.comp_map[comp_name]
+    orig_value = getattr(comp, attribute)
+    setattr(comp, attribute, new_value)
+
+    new_results = sys.solve("price_taker")
+
+    if orig_results.loc[0, "objective"] < new_results.loc[0, "objective"]:
+        print(
+            f"Objective increased when {comp_name} {attribute} was set to {new_value}, "
+            f"implying that there is economic motivation to violate {attribute} constraint"
+        )
+    else:
+        print(
+            f"Objective did not increase when {comp_name} {attribute} was set to {new_value}, "
+            f"implying that there is a lack of economic motivation to violate {attribute} constraint"
+        )
+
+    setattr(comp, attribute, orig_value)
---
+++ examples/variable_inputs_test.py
@@ -0,0 +1,197 @@
+# Copyright 2024, Battelle Energy Alliance, LLC
+# ALL RIGHTS RESERVED
+
+"""
+This script demonstrates a case where input time series vary greatly
+"""
+
+import numpy as np
+from check_constraints_working import (
+    max_capacity_profile_working,
+    max_charge_rate_working,
+    max_discharge_rate_working,
+    periodic_storage_working,
+    ramp_freq_working,
+    ramp_limit_working,
+    rte_working,
+)
+
+import dove.core as dc
+
+
+def run_test():
+    # Set up time series data
+
+    # Electricity demand profile with high variability
+    elec_demand_profile = np.array(
+        [
+            160,
+            140,
+            120,
+            100,
+            80,
+            60,
+            40,
+            20,
+            40,
+            80,
+            140,
+            200,
+            260,
+            320,
+            360,
+            400,
+            380,
+            340,
+            300,
+            260,
+        ]
+    )
+
+    # Scale electricity price on demand
+    scale = 1.0
+    elec_price_profile = np.multiply(elec_demand_profile, scale)
+
+    # Super volatile wind profile
+    wind_max_capacity_factor_profile = np.array(
+        [
+            0.75,
+            0.00,
+            0.43,
+            0.68,
+            0.00,
+            0.19,
+            0.57,
+            0.00,
+            0.81,
+            0.00,
+            0.34,
+            0.27,
+            0.49,
+            0.00,
+            0.62,
+            0.55,
+            0.23,
+            0.78,
+            0.11,
+            0.30,
+        ]
+    )
+    wind_max_capacity_profile = np.multiply(wind_max_capacity_factor_profile, 100)
+
+    # Smooth solar profile, but only producing half the time
+    solar_capacity_profile = np.array(
+        [
+            0,
+            0,
+            0,
+            0,
+            0,
+            15,
+            35,
+            60,
+            90,
+            100,
+            100,
+            90,
+            60,
+            35,
+            15,
+            0,
+            0,
+            0,
+            0,
+            0,
+        ]
+    )
+
+    # Set up resources
+    electricity = dc.Resource(name="electricity")
+    steam = dc.Resource(name="steam")
+
+    wind = dc.Source(
+        name="wind",
+        produces=electricity,
+        max_capacity_profile=wind_max_capacity_profile,
+    )
+
+    solar = dc.Source(
+        name="solar",
+        produces=electricity,
+        max_capacity_profile=solar_capacity_profile,
+    )
+
+    smr = dc.Source(
+        name="smr",
+        produces=steam,
+        max_capacity_profile=np.full(len(elec_demand_profile), 200),
+        cashflows=[dc.Cost(name="vom", alpha=2)],
+    )
+
+    generator = dc.Converter(
+        name="generator",
+        consumes=[steam],
+        produces=[electricity],
+        max_capacity_profile=np.full(len(elec_demand_profile), 300),
+        capacity_resource=steam,
+        ramp_limit=0.4,
+        ramp_freq=2,
+        transfer_fn=dc.RatioTransfer(input_res=steam, output_res=electricity, ratio=0.9),
+    )
+
+    steam_storage = dc.Storage(
+        name="steam_storage",
+        resource=steam,
+        max_charge_rate=0.4,
+        max_discharge_rate=0.4,
+        rte=0.9,
+        max_capacity_profile=np.full(len(elec_demand_profile), 300),
+        initial_stored=0.5,
+    )
+
+    battery = dc.Storage(
+        name="battery",
+        resource=electricity,
+        max_charge_rate=0.75,
+        max_discharge_rate=0.75,
+        rte=0.8,
+        max_capacity_profile=np.full(len(elec_demand_profile), 100),
+        initial_stored=0.5,
+    )
+
+    grid = dc.Sink(
+        name="grid",
+        consumes=electricity,
+        max_capacity_profile=elec_demand_profile,
+        flexibility="flex",
+        cashflows=[dc.Revenue(name="elec_sales", price_profile=elec_price_profile)],
+    )
+
+    sys = dc.System(
+        components=[wind, solar, smr, generator, steam_storage, battery, grid],
+        resources=[steam, electricity],
+        time_index=np.arange(0, 20),
+    )
+
+    results = sys.solve("price_taker")
+
+    with open("variable_inputs_test_results.csv", "w") as f:
+        f.write(results.to_csv())  # Easiest way to view the results is as a csv
+    print(results)
+
+    # Confirm that constraints are not being violated
+    max_capacity_profile_working(sys, results, "smr", "smr_steam_produces")
+    max_capacity_profile_working(sys, results, "battery", "battery_SOC")
+    max_capacity_profile_working(sys, results, "wind", "wind_electricity_produces")
+
+    ramp_limit_working(sys, results, "generator", "generator_steam_consumes")
+    ramp_freq_working(sys, results, "generator", "generator_steam_consumes")
+
+    max_charge_rate_working(sys, results, "battery")
+    max_discharge_rate_working(sys, results, "battery")
+    rte_working(sys, results, "battery")
+    periodic_storage_working(sys, results, "battery")
+
+
+if __name__ == "__main__":
+    run_test()
